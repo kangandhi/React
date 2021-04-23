1b0ceb40132032d5047665f89591c0382c717be3
kangandhi <81741400+kangandhi@users.noreply.github.com>
2021-04-23 17:53:05 +0900

    Update of report on react_2021/04/23_01

---
+++ todos-list/src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,22 @@
+//this is second custom components 
+// what that component is all about 
+// this component will be about rendering the Date of expense.
+import React from 'react';
+
+import './ExpenseDate.css';
+
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
